0ffb454b697101741e1c51b6e4f06507347d1af6
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-11-22 15:35:33 -0600

    Add Time Class, Add update Function, Add Fps, Add Graphics2D Add window background color

--- src/com/jenkins/pong/MainDriver.java
+++ src/com/jenkins/pong/MainDriver.java
@@ -3,7 +3,7 @@ package com.jenkins.pong;
 public class MainDriver {
     public static void main(String[] args){
         Window window = new Window();
-        Thread t1 = new Thread();
+        Thread t1 = new Thread(window);
         t1.start();
     }
 }
--- src/com/jenkins/pong/Window.java
+++ src/com/jenkins/pong/Window.java
@@ -1,8 +1,11 @@
 package com.jenkins.pong;
 
 import javax.swing.JFrame;
-
+import java.awt.Graphics2D;
+import java.awt.Color;
 public class Window extends JFrame implements Runnable{
+
+    Graphics2D g2;
 
     public Window(){
         /**
@@ -13,9 +16,33 @@ public class Window extends JFrame implements Runnable{
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // ability to close window
         this.setTitle(Constants.SCREEN_TITLE);
+
+        g2 = (Graphics2D)this.getGraphics(); // returns graphics obj
+    }
+
+    public void update(double dt) {
+        //Drawing screen background
+        g2.setColor(Color.BLACK);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+
+//        System.out.println("" + dt + " seconds passed since the last frame" );
+        System.out.println(1 / dt + " fps");
+
     }
     public void run() {
+        double lastFrameTime = 0.0;
         while (true){
+            double time = Time.getTime();
+            double deltaTime = time - lastFrameTime;
+            lastFrameTime = time;
+
+            update(deltaTime);
+            //Capping FPS
+            try{
+                Thread.sleep(30);
+            }catch (Exception e){
+
+            }
 
         }
     }
